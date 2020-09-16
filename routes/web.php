75733a5ad1e8aca29d5f903ef6bc56d584d9dbf1
helnania234 <helnakurniawati91@gmail.com>
2020-09-16 14:25:03 +0700

    data siswa belum jadi

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/siswa', [App\Http\Controllers\SiswaController::class, 'index'])->name('index');
 
 Auth::routes();
 
